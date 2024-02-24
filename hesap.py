1f478d3a9a7b2dbb235517cd939ee14ace53a02c
Fatih Mehmet Ata <faata@k1m07s11.42istanbul.com.tr>
2024-02-24 13:49:00 +0300

    .

--- hesap.py
+++ hesap.py
@@ -2,10 +2,10 @@ import tkinter as tk
 import ctypes
 from sympy import sympify
 
-# Load the shared library
-lib = ctypes.CDLL("./hesap.so")
+lib = ctypes.CDLL("./hesap.so") #static object dosyamızı dahil ediyoruz.
 
-# Define function signatures (double arguments and double return type)
+
+#burada da fonksiyonlarımızın tanımını ve atamalarını yapıyoruz.
 add_func = lib.add
 add_func.argtypes = [ctypes.c_double, ctypes.c_double]
 add_func.restype = ctypes.c_double
@@ -22,18 +22,15 @@ divide_func = lib.divide
 divide_func.argtypes = [ctypes.c_double, ctypes.c_double]
 divide_func.restype = ctypes.c_double
 
-# Initialize window and variables
 window = tk.Tk()
 window.title("Calculator with ctypes")
 window.geometry("400x200")
 
 current = ""
 
-# Text input field
 text_input = tk.Entry(window, width=35, borderwidth=5)
 text_input.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-# Define button click functions
 def button_click(char):
     global current
     current += str(char)
@@ -52,9 +49,7 @@ def button_clear():
 def button_equal():
     global current
     try:
-        # Convert expression to float numbers before calling C functions
-        parsed_exp = sympify(current.replace('=', ""))
-        # Update the input field with the calculated result
+        parsed_exp = sympify(current.replace('=', "")) #sympify ile sadece ayırmak ile kalmıyoruz işlemleri de yapıyoruz (gayet kullanışlı).
         current = str(parsed_exp)
         text_input.delete(0, tk.END)
         text_input.insert(0, current)
@@ -62,7 +57,7 @@ def button_equal():
         current = "ERROR"
         text_input.delete(0, tk.END)
         text_input.insert(0, current)
-# Buttons
+
 buttons = [
     "7", "8", "9", "+",
     "4", "5", "6", "-",
@@ -79,5 +74,4 @@ for button_text in buttons:
         col_value = 0
         row_value += 1
 
-# Run the window
 window.mainloop()
